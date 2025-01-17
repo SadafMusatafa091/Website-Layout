40abe1f65400f253e4b28f3e984ddfc7a0a5406b
SadafMusatafa091 <sm8610079@gmail.com>
2025-01-17 11:14:32 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Example function to display an alert
+function displayMessage() {
+    alert("Welcome to my website!");
+}
+
+// Call the function when the page loads
+window.onload = function() {
+    displayMessage();
+};
